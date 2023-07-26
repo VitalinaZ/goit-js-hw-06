5fcae19ed42579c42cd9db263572ed375b3db24c
VitalinaZ <vitalina.zakrevska@gmail.com>
2023-07-27 00:53:36 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {elements: { email, password }
+  } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+    return console.dir("Please fill in all the fields!");
+    }
+    console.dir(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
