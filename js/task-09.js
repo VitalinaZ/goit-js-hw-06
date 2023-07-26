75fd693817bdd2760f40839886cdb7fe825d1485
VitalinaZ <vitalina.zakrevska@gmail.com>
2023-07-27 01:52:05 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector("body");
+console.dir(body);
+const changeColorBtn = document.querySelector(".change-color")
+const nameColor = document.querySelector(".color")
+changeColorBtn.addEventListener("click", onChangeColorBtn);
+function onChangeColorBtn(evt) {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  nameColor.textContent = newColor;
+};
